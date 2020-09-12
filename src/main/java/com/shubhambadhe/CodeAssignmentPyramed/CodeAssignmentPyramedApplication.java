ea83cd8e110b3f0fdfb1885c62da87917c563327
Shubham Badhe <skb2496@gmail.com>
2020-09-12 11:48:24 +0530

    Initial commit

---
+++ src/main/java/com/shubhambadhe/CodeAssignmentPyramed/CodeAssignmentPyramedApplication.java
@@ -0,0 +1,13 @@
+package com.shubhambadhe.CodeAssignmentPyramed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeAssignmentPyramedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeAssignmentPyramedApplication.class, args);
+	}
+
+}
